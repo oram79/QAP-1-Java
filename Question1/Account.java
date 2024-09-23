a9bd2594de67c7aa34e811239029fa30a8e37c10
Logan Oram <logan.oram@keyin.com>
2024-09-23 15:42:39 -0230

    variables and Constructors

--- Question1/Account.java
+++ Question1/Account.java
@@ -2,4 +2,22 @@ package Question1;
 
 public class Account {
     
+    //Private Variables
+    private String id;
+    private String name;
+    private int balance = 0;
+
+    //Fist Constructor
+    public Account(String id, String name)
+    {
+        this .id = id;
+        this.name = name;
+    }
+    //Second Constructor
+    public Account(String id, String name, int balance)
+    {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
 }
