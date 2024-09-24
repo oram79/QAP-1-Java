52a99215a3696914664a50da00b9cbf51a8073d1
Logan Oram <logan.oram@keyin.com>
2024-09-24 19:55:29 -0230

    output complete

--- Question2/TestDate.java
+++ Question2/TestDate.java
@@ -1,5 +1,14 @@
 package Question2;
 
 public class TestDate {
-    
+    public static void main(String[] args) {
+        Date myDate = new Date(23, 04, 2003);
+        System.out.println("Date:" + myDate.toString());
+
+        myDate.setDay(28);
+        myDate.setMonth(04);
+        myDate.setYear(2003);
+
+        System.out.println("New Date:" + myDate.toString());
+    }
 }
