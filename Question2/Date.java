69362d2c60757d8508a3b6a2e695806a8aaf3953
Logan Oram <logan.oram@keyin.com>
2024-09-24 19:42:42 -0230

    constructor/getters/setters

--- Question2/Date.java
+++ Question2/Date.java
@@ -1,5 +1,35 @@
 package Question2;
 
 public class Date {
-    
+    private int day;
+    private int month;
+    private int year;
+
+    //Constructor
+    public Date(int day, int month, int year)
+    {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public int getYear() {
+        return year;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
