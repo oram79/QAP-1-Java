9ce313393031896b2493d0850351516b0c0e21f7
Logan Oram <logan.oram@keyin.com>
2024-09-24 20:21:13 -0230

    question3 complete

--- Question3/TestTime.java
+++ Question3/TestTime.java
@@ -1,5 +1,21 @@
 package Question3;
 
 public class TestTime {
-    
+    public static void main(String[] args) {
+    // Two Objects
+    Time t1 = new Time(21, 10, 15);
+    Time t2 = new Time(10, 20, 25);
+
+    // Starting Times
+    System.out.println("T1 Time: " + t1);
+    System.out.println("T2 Time: " + t2);
+
+    // nextSecond() for t1
+    t1.nextSecond();
+    System.out.println("nextSecond t1: " + t1);
+
+    // previousSecond() for t2
+    t2.previousSecond();
+    System.out.println("previousSecond t2: " + t2);
+    }
 }
