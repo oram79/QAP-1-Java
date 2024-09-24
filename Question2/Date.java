7822456404be5780a940f3ce3517b6c976a1d891
Logan Oram <logan.oram@keyin.com>
2024-09-24 19:48:32 -0230

    setDate/toString

--- Question2/Date.java
+++ Question2/Date.java
@@ -32,4 +32,14 @@ public class Date {
     public void setYear(int year) {
         this.year = year;
     }
+
+    public void setDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String toString() {
+        return String.format("%02d/%02d/%04d", day, month, year);
+    }
 }
